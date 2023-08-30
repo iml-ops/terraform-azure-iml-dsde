d801658c76a9bdae03b312d51d08e4c65df2df78
Miles <miles.smith@infiniaml.com>
2023-08-30 15:09:33 -0400

    fix: added scopes to output

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,7 @@ output "dsde_config" {
     network_cidr         = var.network_cidr
     boundary_cluster_id  = var.boundary_cluster_id
     azure_resource_group = azurerm_resource_group.this
+    authorized_scopes    = var.sensitive_scopes
     sp_config = base64encode(jsonencode({
       client_id     = azuread_service_principal.deployer.application_id
       client_secret = azuread_service_principal_password.deployer.value
